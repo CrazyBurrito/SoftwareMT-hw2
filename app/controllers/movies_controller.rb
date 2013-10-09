3a257968c758493a10b1f0c5b38b10f6e8f9ef86
CzacasticJack <joshua.weaver@colorado.edu>
2013-10-09 16:58:50 -0700

    Almost completed parts 2,3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,27 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.order(params[:sort_param])
+    sort = params[:sort] || session[:sort]
+    case sort
+    when 'title'
+      ordering,@title_header = {:order => :title}, 'hilite'
+    when 'release_date'
+      ordering,@date_header = {:order => :release_date}, 'hilite'
+    end
+    @all_ratings = Movie.all_ratings
+    @checked_ratings = params[:ratings] || session[:ratings] || {}
+
+    if params[:sort] != session[:sort]
+      session[:sort] = sort
+      redirect_to :sort => sort, :ratings => @checked_ratings and return
+    end
+
+    if params[:ratings] != session[:ratings] and @checked_ratings != {}
+      session[:sort] = sort
+      session[:ratings] = @checked_ratings
+      redirect_to :sort => sort, :ratings => @checked_ratings and return
+    end
+    @movies = Movie.find_all_by_rating(@checked_ratings.keys, ordering)
   end
 
   def new
